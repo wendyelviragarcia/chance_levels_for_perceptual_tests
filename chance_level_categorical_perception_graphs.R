a1e628a582efaf2f4942058cb9408d8d65ffa812
wendyelviragarcia <wendyelviragarcia@gmail.com>
2019-12-17 17:43:53 +0100

    Update chance_level_categorical_perception_graphs.R

--- chance_level_categorical_perception_graphs.R
+++ chance_level_categorical_perception_graphs.R
@@ -7,7 +7,7 @@ library(readxl)
 myData <- as.data.frame(read_excel("C:/Users/labfonub99/OneDrive - UNED/2020-CIFE/0paper_WAPA/grafico_11_valencia_datos_reconstruidos.xlsx"))
 
 twoOptions <- 0.5
-numAnswers <- 100
+nAnswers <- 100
 yourResult <- 50
 
 
@@ -18,12 +18,13 @@ myData$Answer =as.factor(myData[,2])
 myData$nAnswers =as.numeric(myData[,3])
 
 ##############
-# using Bernoulli trials, We can test if your success score is beyond chance level
+# using Bernoulli trials, We can test if your success score is beyond chance level.
+# returns the probability of your result
 #############
 # pbinom in r - binomial probability r
 # cumulative probability of an event
 
-pbinom(yourResult,nAnswers,0.5)
+pbinom(yourResult,nAnswers,0.5,lower.tail=FALSE)
 
 #the number of trials is not taken into account, it can be with rbinom(nTrials = 20)
 # Example: If we flip a coin 120 times (nOfListeners), what is the probability of getting 40% (yourResults) or less heads?
@@ -43,9 +44,8 @@ pbinom(yourResult,nAnswers,0.5)
     #qbinom(0.75,numAnswers,.5)
     #lowerLimit
     #qbinom(0.25,numAnswers,.5)
-    
+    #we test when our data becomes different of a probability of 50% 
     #compute upper limit
-    numAnswers <- 100
     possResult <-numAnswers-1
     pValue = 0
     while ((possResult > 0) & (pValue< 0.05)) {
@@ -67,14 +67,14 @@ pbinom(yourResult,nAnswers,0.5)
 
     
     ##############  ##############
-    # But there is a function for it
+    # 
     #############  ##############
-    # you can have in one line the "inverse" of pbinom. 
+    # 
     # You tell the function the percentile you want (25% = 0.25) and the function generates the number of successes associated
     #to that cumulative distribution. For example, 
     #qbinom(p, size = , prob = , lower.tail = FALSE, log.p = FALSE)
     # 25% limit of the distribution, for 1000 trials, the outcome is dichotomic (yes/no "a" or "b"), so probability 0.5
-    qbinom(0.25,1000,0.5)
+    qbinom(0.25,100,0.5)
 
 
 
@@ -91,6 +91,6 @@ p= ggplot(data=myData, aes(x=Stimuli, y=nAnswers, group=Answer)) +
  
 
 # we add a red/transparent red ribbon with the limits of choice levels
-p + geom_ribbon(aes(ymin=qbinom(0.25,numAnswers,twoOptions), ymax=qbinom(0.75,numAnswers,twoOptions)), fill = "red", alpha=0.2)+ylab("Cases of identification per category")
+p + geom_ribbon(aes(ymin=lowerLimit, ymax=upperLimit), fill = "red", alpha=0.2)+ylab("Cases of identification per category")
 
 
